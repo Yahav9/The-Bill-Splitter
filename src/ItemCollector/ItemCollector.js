69112f3bb2166d7201ac79c54cc9d89d5364db98
Yahav <yahav9@cyber4s.dev>
2022-12-16 11:34:16 +0200

    Refactor - delete ItemsList folder and NamesList folder

--- src/ItemCollector/ItemCollector.js
+++ src/ItemCollector/ItemCollector.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ItemCollector.scss';
-import ItemsList from './ItemsList/ItemsList';
+import Item from '../shared/Item/Item';
+import ItemForm from './ItemForm/ItemForm';
+import Card from '../shared/Card/Card';
+import Button from '../shared/Button/Button';
 
 function ItemCollector(props) {
+    const [items, setItems] = useState([]);
+
+    const createItem = (event, name, price, index) => {
+        event.preventDefault();
+        name.length < 1 && (name = 'Item');
+        const newItem = { name, price, index };
+        setItems(items => [...items, newItem]);
+    };
+
+    const nextClickHandler = () => {
+        localStorage.clear();
+        localStorage.setItem('storedItems', JSON.stringify(items));
+        props.onNextClick();
+    };
+
     return (
         <div className="item-collector">
-            <h2>Give Me Every Item on the Bill</h2>
-            <ItemsList
-                onNextClick={props.onNextClick}
-            />
+            <h1>Give Me Every Item on the Bill</h1>
+            <ul className="items-list">
+                {
+                    items.length > 0 && items.map(item => {
+                        return (
+                            <li key={item.index}>
+                                <Item
+                                    name={item.name}
+                                    price={item.price}
+                                />
+                            </li>
+                        );
+                    })
+                }
+                <li>
+                    <Card className="item total">
+                        <h3>Total: </h3>
+                        <h3>{items.reduce((a, b) => Number(a) + Number(b.price), 0).toFixed(2)}₪</h3>
+                    </Card>
+                </li>
+                <ItemForm onAdd={createItem} />
+                <Button
+                    className="next-button"
+                    onClick={nextClickHandler}
+                    disabled={items.length < 1}
+                >NEXT</Button>
+            </ul>
         </div>
     );
 }
--- src/NamesCollector/NamesCollector.js
+++ src/NamesCollector/NamesCollector.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NamesCollector.scss';
-import NamesList from './NamesList/NamesList';
+import Person from '../shared/Person/Person';
+import NameForm from './NameForm/NameForm';
+import Button from '../shared/Button/Button';
 
 function NamesCollector(props) {
+    const [people, setPeople] = useState([]);
+
+    const createName = (event, name, index) => {
+        event.preventDefault();
+        const newPerson = { name, payment: 0, index };
+        setPeople(people => [...people, newPerson]);
+    };
+
+    const nextClickHandler = () => {
+        localStorage.setItem('storedPeople', JSON.stringify(people));
+        props.onNextClick();
+    };
+
     return (
         <div className="names-collector">
             <h1>Who's Here?</h1>
-            <NamesList
-                onNextClick={props.onNextClick}
-            />
+            <ul className="names-list">
+                {
+                    people.length > 0 && people.map(person => {
+                        return <Person
+                            key={person.index}
+                            name={person.name}
+                        />;
+                    })
+                }
+            </ul>
+            <NameForm onAdd={createName} />
+            <Button
+                className="next-button"
+                onClick={nextClickHandler}
+                disabled={people.length < 2}
+            >Next</Button>
         </div>
     );
 }
